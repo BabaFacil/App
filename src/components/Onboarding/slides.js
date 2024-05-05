725a280963d6b134991acc678e39d4c6878fe89c
Pedro-Rbeiro <business.psycho@outlook.com>
2024-05-05 00:19:48 -0300

    feat: Adição da tela welcome

---
+++ src/components/Onboarding/slides.js
@@ -0,0 +1,28 @@
+// se fizer alguma alteração será necessario dar reload
+export default [
+    {
+        id: "1",
+        title: "1 screen",
+        description: "1 screen description",
+        image: require("../../assets/imgs/logo-text2.png")
+    },
+    {
+        id: "2",
+        title: "2 screen",
+        description: "2 screen description",
+        image: require("../../assets/imgs/logo-text2.png")
+    },
+    {
+        id: "3",
+        title: "3 screen",
+        description: "3 screen description",
+        image: require("../../assets/imgs/logo-text2.png")
+    },
+    {
+        id: "4",
+        title: "4 screen",
+        description: "4 screen description",
+        image: require("../../assets/imgs/logo-text2.png")
+    },
+
+]
