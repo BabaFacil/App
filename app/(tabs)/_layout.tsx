d282b727e574fc44583fc2615ffe454061aedab7
Pedro-Rbeiro <business.psycho@outlook.com>
2024-07-06 14:36:42 -0300

    :bug: fix: All routes showing on apk deploy

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,79 +1,17 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import { Stack, usePathname } from 'expo-router';
+
+import MenuBar from '@/components/MenuBar';
 
 export default function TabLayout() {
-
+  const currentTab = usePathname().substring(1).toLowerCase()
+  
   return (
-    <Tabs screenOptions={{
-      tabBarActiveTintColor: '#E96181', tabBarInactiveTintColor: '#C8C8C8', headerShown: false, tabBarStyle: {
-        borderRadius: 15,
-      }, tabBarLabelStyle: {
-        fontSize: 12
-      }
-    }}>
-      <Tabs.Screen
-        name="Home/index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="Activitys/index"
-        options={{
-          title: 'Activitys',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="list-ul" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="PreChat/index"
-        options={{
-          title: 'Chat',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="wechat" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="Settings/index"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-        }}
-      />
-
-      {/*
-      Exclude routes.
-      This seems not to be the smartest way
-      */}
-      <Tabs.Screen
-        name="PreChat/Chat/[id]"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="Settings/Account/index"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="Settings/Account/AccountInfo/index"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="Settings/Account/AccountInfo/AccountEdit/[edit]"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="Settings/DarkModeSwitch/index"
-        options={{
-          href: null,
-        }}
-      />
-    </Tabs>
+    <>
+      <Stack screenOptions={{
+        headerShown: false,
+        animation: "none"
+      }} />
+      <MenuBar TelaAtiva={currentTab}/>
+    </>
   );
 }
